967f0f3e4328449d3f1f8999d2a0f1f8e54bad15
Veronika Portnyh <bezpalevo08@mail.ru>
2024-12-19 15:48:39 +0400

    feat: add script for fade-block

--- js/skill-animation.js
+++ js/skill-animation.js
@@ -3,17 +3,17 @@ const listSkills = document.querySelector('#listSkills');
 const toggleCards = (event) => {
     const isCard = event.target.closest('[data-skill-card]');
     const isCross = event.target.closest('[data-cross]');
-    // найти фейд блок
+    const fadeBlock = document.querySelector('#fadeBlock');
 
     if (isCard) {
         isCard.classList.add('active');
-        // добавить класс active на фейд блок
+        fadeBlock.classList.add('active');
     }
 
     if (isCross) {
         const card = isCross.closest('[data-skill-card]');
         card.classList.remove('active');
-        // удалить класс active с фейд блока
+        fadeBlock.classList.remove('active');
     }
 }
 
