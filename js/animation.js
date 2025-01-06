179e425583f63a9f3cc37a4e99616c020af4f4dd
Veronika Portnyh <bezpalevo08@mail.ru>
2025-01-06 11:43:41 +0400

    feat: add animation for about me

--- js/animation.js
+++ js/animation.js
@@ -1,6 +1,32 @@
+gsap.registerPlugin(ScrollTrigger); 
+
 gsap.from('#hero', {
     y: -400,
     duration: 2,
     opacity: 0,
-    ease: 'bounce'
+    // ease: 'bounce'
+})
+
+gsap.from('#aboutIcon',{
+    opacity:0,
+    x: -200,
+    scrollTrigger:{
+        // markers: true,
+        trigger: '#aboutMe',
+        start: 'top 70%',
+        end:'30% 50%',
+        scrub: 3,
+    }
+})
+
+gsap.from('#aboutText',{
+    opacity:0,
+    x: 300,
+    scrollTrigger:{
+        // markers: true,
+        trigger: '#aboutMe',
+        start: 'top 70%',
+        end:'30% 50%',
+        scrub: 3,
+    }
 })
